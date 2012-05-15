cb91a00253079d2db57a2fd7d97e8a7afdcb06f1
Arne Krämer <PixelPunk@marc-kampss-macbook.local>
2012-05-15 18:00:35 +0200

    added changes to user mvc etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SYMP::Application.routes.draw do
+  resources :users
+
   resources :ratings
 
   resources :skills
-
-  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
