3c88b97eaf470eb3b54ca3d76590f4d19d6afce0
Arne Krämer <arne.kraemer@gmx.de>
2012-06-05 21:45:23 +0200

    user actions are now limited to own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @skills = Skill.where('user_id = ?', params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
