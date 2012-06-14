14a0fff6b183360dee7423edb7b107ced6e0dff4
Alois <al0is@me.com>
2012-06-14 16:05:54 +0200

    a tiny merge
    Schema version change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120612211414) do
+ActiveRecord::Schema.define(:version => 20120608194618) do
 
   create_table "posts", :force => true do |t|
     t.string   "subject"
@@ -41,17 +41,6 @@ ActiveRecord::Schema.define(:version => 20120612211414) do
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
   end
-
-  create_table "relationships", :force => true do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-  add_index "relationships", ["followed_id"], :name => "index_relationships_on_followed_id"
-  add_index "relationships", ["follower_id", "followed_id"], :name => "index_relationships_on_follower_id_and_followed_id", :unique => true
-  add_index "relationships", ["follower_id"], :name => "index_relationships_on_follower_id"
 
   create_table "roles", :force => true do |t|
     t.integer  "user_id"
