d9331bb1170e68edb2a87b12da70b68c4a7713dd
Alois <al0is@me.com>
2012-06-19 10:44:06 +0200

    Removed "my topics" for admin. Added Captions for tables.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120612211414) do
+ActiveRecord::Schema.define(:version => 20120618175001) do
 
   create_table "posts", :force => true do |t|
     t.string   "subject"
@@ -28,8 +28,10 @@ ActiveRecord::Schema.define(:version => 20120612211414) do
     t.string   "subject"
     t.text     "message"
     t.boolean  "unread"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                           :null => false
+    t.datetime "updated_at",                           :null => false
+    t.boolean  "sender_deleted",    :default => false
+    t.boolean  "recipient_deleted", :default => false
   end
 
   create_table "ratings", :force => true do |t|
